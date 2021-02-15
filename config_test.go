a2e3f082d9a4e10463a5d5023d97231bd208ca95
Niteesh Hegde <niteeshhegde@gmail.com>
2021-02-15 09:28:12 +0530

    ref

--- config_test.go
+++ config_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -40,10 +39,28 @@ var fileConfig = Config{
 	},
 }
 
+func arraysAreEqual(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestNewConfig(t *testing.T) {
 	t.Run("default config", func(t *testing.T) {
 		config, err := newConfig("")
-		if !reflect.DeepEqual(config, defaultConfig) {
+		if config.Type.AcceptExtra != defaultConfig.Type.AcceptExtra || config.Type.Required != defaultConfig.Type.Required || config.Type.MinLength != defaultConfig.Type.MinLength || config.Type.MaxLength != defaultConfig.Type.MaxLength || !arraysAreEqual(config.Type.Values, defaultConfig.Type.Values) {
+			t.Errorf("Default Config Type was not created properly.\n Expected ---> %v, \n Recieved ---> %v", defaultConfig, config)
+		}
+		if config.Scope.AcceptExtra != defaultConfig.Scope.AcceptExtra || config.Scope.Required != defaultConfig.Scope.Required || config.Scope.MinLength != defaultConfig.Scope.MinLength || config.Scope.MaxLength != defaultConfig.Scope.MaxLength || !arraysAreEqual(config.Scope.Values, defaultConfig.Scope.Values) {
+			t.Errorf("Default Config Scope was not created properly.\n Expected ---> %v, \n Recieved ---> %v", defaultConfig, config)
+		}
+		if config.Description != defaultConfig.Description || config.Body != defaultConfig.Body || config.Footer != defaultConfig.Footer {
 			t.Errorf("Default Config was not created properly.\n Expected ---> %v, \n Recieved ---> %v", defaultConfig, config)
 		}
 		if err != nil {
@@ -53,8 +70,14 @@ func TestNewConfig(t *testing.T) {
 
 	t.Run("custom config", func(t *testing.T) {
 		config, err := newConfig("commit.json")
-		if !reflect.DeepEqual(config, fileConfig) {
-			t.Errorf("Config from commit.json was not created properly.\n Expected ---> %v, \n Recieved ---> %v", defaultConfig, config)
+		if config.Type.AcceptExtra != fileConfig.Type.AcceptExtra || config.Type.Required != fileConfig.Type.Required || config.Type.MinLength != fileConfig.Type.MinLength || config.Type.MaxLength != fileConfig.Type.MaxLength || !arraysAreEqual(config.Type.Values, fileConfig.Type.Values) {
+			t.Errorf("File Config Type was not created properly.\n Expected ---> %v, \n Recieved ---> %v", fileConfig, config)
+		}
+		if config.Scope.AcceptExtra != fileConfig.Scope.AcceptExtra || config.Scope.Required != fileConfig.Scope.Required || config.Scope.MinLength != fileConfig.Scope.MinLength || config.Scope.MaxLength != fileConfig.Scope.MaxLength || !arraysAreEqual(config.Scope.Values, fileConfig.Scope.Values) {
+			t.Errorf("File Config Scope was not created properly.\n Expected ---> %v, \n Recieved ---> %v", fileConfig, config)
+		}
+		if config.Description != fileConfig.Description || config.Body != fileConfig.Body || config.Footer != fileConfig.Footer {
+			t.Errorf("File Config was not created properly.\n Expected ---> %v, \n Recieved ---> %v", fileConfig, config)
 		}
 		if err != nil {
 			t.Errorf("Error while creating Config from file %s", err)
